d95f71b4ecef6b77a4f39de8dea45d401e0aa381
Ann Wanjeri <wannjeri2@gmail.com>
2022-05-05 00:49:16 +0300

    created functions to get articles and to process them from json format

--- app/requests.py
+++ app/requests.py
@@ -45,8 +45,33 @@ def process_new_sources(sources_list):
         description = one_source.get("description")
         url = one_source.get("url")
         category = one_source.get("category")
-        language = one_source.get("language")
         country = one_source.get("country")        
-        new_source = Sources(id,name,description,url,category,language,country)
+        new_source = Sources(id,name,description,url,category,country)
         sources_outcome.append(new_source)    
     return sources_outcome
+
+def get_articles(article):    
+        articles_url = articles_url.format(article,api_key)
+    
+        with urllib.request.urlopen(articles_url) as url:
+            articles_data = url.read()
+            articles_response = json.loads(articles_data)        
+            articles_outcome = None        
+            if articles_response['articles']:
+                articles_outcome_items = articles_response['articles']
+                articles_outcome = process_new_articles(articles_outcome_items)
+        return articles_outcome
+
+def process_new_articles(articles_list):
+    articles_outcome = []    
+    for one_article in articles_list:
+        source = one_article.get("source")
+        author = one_article.get("author")
+        description = one_article.get("description")
+        title = one_article.get("title")
+        url = one_article.get("url")
+        urlToImage = one_article.get("urlToImage")
+        publishedAt = one_article.get("publishedAt")
+        new_article = Articles(source, author, title, description, url, urlToImage, publishedAt)
+        articles_outcome.append(new_article)    
+    return articles_outcome
