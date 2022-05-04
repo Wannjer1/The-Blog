99fb09082d6b2a78c179d16aa9db6f4c204a37fe
Ann Wanjeri <wannjeri2@gmail.com>
2022-05-05 00:26:16 +0300

    created the articles review class

--- app/models.py
+++ app/models.py
@@ -8,3 +8,26 @@ class Articles:
         self.url = url
         self.urlToImage= urlToImage
         self.publishedAt = publishedAt
+
+class ArticleReview:
+    articles_reviews = []
+
+    def __init__(self,title,urlToImage,review):
+        self.title = title
+        self.urlToImage = urlToImage
+        self.review = review 
+
+    def save_artcles_review(self):
+        ArticleReview.articles_reviews.append(self)
+
+    @classmethod
+    def clear_articles_reviews(cls):
+        ArticleReview.articles_reviews.clear()
+
+    @classmethod
+    def get_articles_reviews(cls,title):        
+        article_results = []        
+        for review in cls.articles_reviews:
+            if review.title == title:
+                article_results.append(review)        
+        return article_results
