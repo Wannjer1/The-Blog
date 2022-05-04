e762cfb0f825063bde6a3f909d184eb5d08cbb9e
Ann Wanjeri <wannjeri2@gmail.com>
2022-05-05 00:34:27 +0300

    created variables to get the api and base url

--- app/requests.py
+++ app/requests.py
@@ -1,2 +1,11 @@
 import urllib.request,json
 from .models import Articles,Sources
+
+
+# getting the api key
+api_key = None
+
+# getting the articles and sources base url
+base_url = None
+articles_url = None
+sources_url = None
