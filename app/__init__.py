c4a011e82f83d451014add4e6e7fc88d2766f75b
Ann Wanjeri <wannjeri2@gmail.com>
2022-05-04 23:58:29 +0300

    imported te config_options dict and created a bootstrap instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,15 @@
 from flask import Flask 
 from flask_bootstrap import Bootstrap
+from config import config_options
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+
+
+    # initializing application
+    app = Flask(__name__,instance_relative_config=True)
+
+    # creating the app configurations
+
+    # initializing Flask extension
