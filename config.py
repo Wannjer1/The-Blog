17d46ede9ec427ea7db2e126678c546c07c70cec
Ann Wanjeri <wannjeri2@gmail.com>
2022-05-04 23:29:07 +0300

    created variables to store the base url and sources articles

--- config.py
+++ config.py
@@ -1,10 +1,16 @@
 from distutils.debug import DEBUG
+import os
+
+from instance.config import NEWS_API_KEY 
 
 
 class Config:
     '''General configuration parent class'''
 
-    pass
+    NEWS_API_BASE_URL =  'https://newsapi.org/v2/sources?category={}&apiKey={}'
+    NEWS_ARTICLES_APL_URL='https://newsapi.org/v2/everything?q={}&apiKey={}'
+    SOURCE_ARTICLES_URL='https://newsapi.org/v2/everything?sources={}&apiKey={}' 
+
 
 class ProdConfig(Config):
     '''Production configuration child class
@@ -20,3 +26,5 @@ class DevConfig(Config):
         Config: The parent configuration class with general configuration settings'''
 
     DEBUG=True
+
+
