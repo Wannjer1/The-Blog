5333d97a4f3fdb0b78f643c1b3bac7b966e15a75
Ann Wanjeri <wannjeri2@gmail.com>
2022-05-05 00:46:12 +0300

    created function to process new sources

--- app/requests.py
+++ app/requests.py
@@ -35,3 +35,18 @@ def get_sources(category):
             sources_outcome_items = response['sources']
             sources_outcome = process_new_sources(sources_outcome_items)
     return sources_outcome
+
+def process_new_sources(sources_list):
+
+    sources_outcome = []    
+    for one_source in sources_list:
+        id = one_source.get("id")
+        name = one_source.get("name")
+        description = one_source.get("description")
+        url = one_source.get("url")
+        category = one_source.get("category")
+        language = one_source.get("language")
+        country = one_source.get("country")        
+        new_source = Sources(id,name,description,url,category,language,country)
+        sources_outcome.append(new_source)    
+    return sources_outcome
